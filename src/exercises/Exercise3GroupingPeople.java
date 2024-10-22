26e317d6abf4ed9fef3c509a5859abc7ab3b4f86
Álvaro Rodríguez García <alvarorgtr@gmail.com>
2024-10-22 23:09:44 +0200

    Added hints

--- src/exercises/Exercise3GroupingPeople.java
+++ src/exercises/Exercise3GroupingPeople.java
@@ -29,6 +29,8 @@ public class Exercise3GroupingPeople {
      */
     public static Map<FoodType, Person> findOldestPersonWithEachFoodAsFavorite(List<Person> people) {
         // FIXME: implement using streams
+        // Hint: there is some trickery involved to return Map<FoodType, Person>. A solution returning
+        // Map<FoodType, Optional<Person>> is acceptable, but the tests won't work.
         return Collections.emptyMap();
     }
 
@@ -55,12 +57,14 @@ public class Exercise3GroupingPeople {
 
     /**
      * Returns a map where the keys are the starting age of an age group, and the values are lists consisting of people
-     * in the age group .
+     * in the age group.
      * @param people the list of people.
+     * @param sizeOfGroup the size of the group.
      * @return the map.
      */
     public static Map<FoodType, Integer> groupPeopleByAgeGroups(List<Person> people, int sizeOfGroup) {
         // FIXME: implement using streams
+        // Hint: the question has a very short solution that plays with integer division, but alternatives can be tricky
         return Collections.emptyMap();
     }
 
